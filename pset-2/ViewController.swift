a0ff5babe490ea645e082ad0e3a1ee046d1be3b3
Jerome Mies <Jerome@jeromes-macbook-air.local>
2017-02-23 14:10:40 +0100

    Initial Commit

---
+++ pset-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pset-2
+//
+//  Created by Jerome Mies on 23/02/17.
+//  Copyright © 2017 Jerome Mies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
